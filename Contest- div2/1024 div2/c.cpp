01c9e58a0a3ef6ee071d800ec8a88fdab9579880
jainDivyansh0211 <div0211jain@gmail.com>
2025-05-15 15:56:23 +0530

    cf 1024 div2 added

--- Contest- div2/1024 div2/c.cpp
+++ Contest- div2/1024 div2/c.cpp
@@ -6,11 +6,55 @@ using namespace std;
 #define pii pair<int,int>
 #define vi vector<int>
 #define vii vector<vector<int>>
+int n;
+
+vii final_matrix;
+int dx[]={-1,0,1,0};
+int dy[]={0,-1,0,1};
+
+int is_valid(int x,int y){
+    if(x>=0 && y>=0 && x<n && y<n && final_matrix[x][y]==-1) return 1;
+    else return 0;
+}
+
 
 void solve()
 {
-    int n;
-    
+    cin>>n;
+    final_matrix.clear();
+    final_matrix.assign(n,vi(n,-1));
+    //n even ends at Left bottom
+    //n odd ends at Right top
+    int xi,yi;
+    int st;
+    int cur=n*n -1;
+    if(n&1){
+        xi=0;
+        yi=n-1;
+        st=0;
+
+    }
+    else{
+        xi=n-1;
+        yi=0;
+        st=2;
+    }
+    final_matrix[xi][yi]=cur;
+    cur--;
+    while(cur>=0){
+        if(!is_valid(xi+dx[st],yi+dy[st])) st=(st+1)%4;
+        xi=xi+dx[st];
+        yi=yi+dy[st];
+        final_matrix[xi][yi]=cur;
+        cur--;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<final_matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     
 }
 
