25d7f68e315aaea846834657ca03e63f12b1960b
jainDivyansh0211 <div0211jain@gmail.com>
2025-05-26 15:52:59 +0530

    here we go 2

--- Div2B ladder AZ/1762B-makeGood-array.cpp
+++ Div2B ladder AZ/1762B-makeGood-array.cpp
@@ -13,7 +13,7 @@ void solve()
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
     for(int i=1;i<n;i++){
-        arr[i]=arr[i]+ (-1*(arr[i]%arr[i-1]));
+        
     }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
